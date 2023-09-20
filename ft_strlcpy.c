d2835a68e79cbcd09226d269fe8e43620a2d1c63
alones-dev <kyliandaumont78@gmail.com>
2023-09-20 18:43:52 +0200

    ft_strlcpy

---
+++ ft_strlcpy.c
@@ -0,0 +1,26 @@
+int ft_strlen(const char *s)
+{
+    int i;
+    
+    i = 0;
+    while (s[i])
+        i++;
+    return (i);
+}
+
+size_t ft_strlcpy(char *dst, const char *src, size_t size)
+{
+    int i;
+
+    i = 0;
+    if (size > 0)
+    {        
+        while (src[i])
+        {
+            dst[i] = src[i];
+            i++;
+        }
+        dst[i] = "\0";
+    }
+    return (ft_strlen(src));
+}

a19b9939543203429bd1802c749d90b210d60fc1
alones-dev <kyliandaumont78@gmail.com>
2023-09-21 18:33:44 +0200

    ft_memcmp

---
+++ ft_memcmp.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+    int i;
+    char *mem1;
+    char *mem2;
+
+    if (n == 0)
+        return (0);
+    i = 0;
+    mem1 = (char *)s1;
+    mem2 = (char *)s2;
+    while (mem1[i] && mem1[i] == mem2[i] && i < n)
+        i++;
+    if (i < n)
+        return ((unsigned char)mem1[i] - (unsigned char)mem2[i]);
+    return (0);
+}

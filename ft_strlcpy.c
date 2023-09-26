f36bc9a714b489cf132f3d71e5a4aabc73bba2c9
alones-dev <kyliandaumont78@gmail.com>
2023-09-26 20:33:21 +0200

    fix problem

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -15,12 +15,12 @@ size_t ft_strlcpy(char *dst, const char *src, size_t size)
     i = 0;
     if (size > 0)
     {        
-        while (src[i])
+        while (src[i] && i < (size - 1))
         {
             dst[i] = src[i];
             i++;
         }
-        dst[i] = "\0";
+        dst[i] = '\0';
     }
     return (ft_strlen(src));
 }

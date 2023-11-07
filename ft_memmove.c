b640fb9786de4105e02d9d3bfdde66145f64a5b6
Kylian Daumont <kdaumont@bess-f3r3s3.clusters.42paris.fr>
2023-11-07 16:06:48 +0100

    reset memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,46 +6,12 @@
 /*   By: kdaumont <kdaumont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 15:12:06 by kdaumont          #+#    #+#             */
-/*   Updated: 2023/11/07 09:06:51 by kdaumont         ###   ########.fr       */
+/*   Updated: 2023/11/07 16:06:27 by kdaumont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
-{
-	int		i;
-	char	*mem;
-	char	*base;
-
-	if (n == 0 || (!dest && !src))
-		return (NULL);
-	i = 0;
-	mem = (char *)dest;
-	base = (char *)src;
-	while (i < n)
-	{
-		mem[i] = base[i];
-		i++;
-	}
-	return (mem);
-}
-
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	int		i;
-	char	*mem1;
-	char	*mem2;
-
-	if (n == 0 || (!dest && !src))
-		return (NULL);
-	mem1 = (char *)dest;
-	mem2 = (char *)src;
-	i = 0;
-	while (i < n)
-	{
-		mem2[i] = mem1[i];
-		i++;
-	}
-	return (mem1);
 }

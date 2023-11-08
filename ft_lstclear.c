4285325d0d67b9da45d8aaaf572454d34a81341b
Kylian Daumont <kdaumont@bess-f4r1s3.clusters.42paris.fr>
2023-11-08 15:56:59 +0100

    add come lst

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: kdaumont <kdaumont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 13:03:40 by kdaumont          #+#    #+#             */
-/*   Updated: 2023/11/08 15:28:57 by kdaumont         ###   ########.fr       */
+/*   Updated: 2023/11/08 15:56:32 by kdaumont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,15 @@
 
 void	ft_lstclear(t_list *lst, void (*del)(void *))
 {
-	//
+	t_list *act;
+	t_list *save;
+
+	act = lst;
+	while (act)
+	{
+		save = act->next;
+		ft_lstdelone(act, del);
+		act = save;
+	}
+	lst = NULL;
 }

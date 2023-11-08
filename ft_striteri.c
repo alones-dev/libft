ad43d0ee1961488d048bd9b6470cfe7a204b78d2
Kylian Daumont <kdaumont@bess-f3r3s3.clusters.42paris.fr>
2023-11-08 09:11:54 +0100

    striteri finish

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,20 +6,18 @@
 /*   By: kdaumont <kdaumont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 16:02:52 by kdaumont          #+#    #+#             */
-/*   Updated: 2023/11/07 16:16:15 by kdaumont         ###   ########.fr       */
+/*   Updated: 2023/11/08 09:11:35 by kdaumont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_striteri(char *s, void (*f)(unsigned int, char *))
+void	*ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
 	int i;
 
 	i = 0;
 	while (s[i])
 	{
-		tbl[i] = f(i, s[i]);
+		(*f)(i, &s[i]);
 		i++;
 	}
-	tbl[i] = '\0';
-	return (tbl);
 }

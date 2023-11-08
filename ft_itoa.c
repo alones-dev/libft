ab9a34920cb248e285aa3d9db61bb7cfc870a8e3
Kylian Daumont <kdaumont@bess-f3r3s3.clusters.42paris.fr>
2023-11-08 10:24:13 +0100

    itoa search neg

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: kdaumont <kdaumont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 11:47:26 by kdaumont          #+#    #+#             */
-/*   Updated: 2023/11/07 16:33:06 by kdaumont         ###   ########.fr       */
+/*   Updated: 2023/11/08 10:23:11 by kdaumont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,8 @@ int	get_len_int(int nb)
 
 	i = 0;
 	nb2 = nb;
+	if (nb < 0)
+		i = 1;
 	while (nb2 != 0)
 	{
 		i++;
@@ -38,13 +40,16 @@ int	get_len_int(int nb)
 	return (i);
 }
 
-void	rev_char(char *tab, int size)
+void	rev_char(char *tab, int size, int neg)
 {
 	int		i;
 	int		temp;
 	float	j;
 
-	i = 0;
+	if (neg == 1)
+		i = 1;
+	else
+		i = 0;
 	j = size / 2;
 	while (i < j)
 	{
@@ -57,17 +62,33 @@ void	rev_char(char *tab, int size)
 
 char	*ft_itoa(int n)
 {
-	char *tbl;
-	int i;
+	char	*tbl;
+	int		i;
+	int		neg;
 
 	i = 0;
 	tbl = malloc(sizeof(char) * get_len_int(n));
+	if (n < 0)
+	{
+		tbl[0] = '-';
+		n = -n;
+		i = 1;
+		neg = 1;
+	}
 	while (n != 0)
 	{
 		tbl[i] = n % 10 + '0';
 		n = n / 10;
 		i++;
 	}
-	rev_char(tbl, ft_strlen(tbl));
+	if (neg == 1)
+		rev_char(tbl, ft_strlen(tbl), 1);
+	else
+		rev_char(tbl, ft_strlen(tbl), 0);
 	return (tbl);
+}
+
+void	main(void)
+{
+	printf("%s\n", ft_itoa(-125));
 }

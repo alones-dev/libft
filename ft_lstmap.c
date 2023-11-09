4934759d6702e74f5d2efdeb7ffd19a3b8e06d15
Kylian Daumont <kdaumont@bess-f3r3s1.clusters.42paris.fr>
2023-11-09 08:57:29 +0100

    test lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,8 +6,26 @@
 /*   By: kdaumont <kdaumont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 13:04:05 by kdaumont          #+#    #+#             */
-/*   Updated: 2023/11/08 13:35:44 by kdaumont         ###   ########.fr       */
+/*   Updated: 2023/11/09 08:55:10 by kdaumont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list *new;
+	t_list *act;
+
+	new = malloc(sizeof(t_list));
+	act = lst;
+	while (act)
+	{
+		new = (*f)(act->content);
+		new = new->next;
+		act = act->next;
+		if (del)
+			ft_lstdelone(new, del);
+	}
+	return (new);
+}
